144e4b5f513b625a66c42cdb1131db7661031c27
Lucas Horsman <lucashorsman@gmail.com>
2026-04-29 11:46:57 -1000

    correct baudrate for canbus

--- start_script.bash
+++ start_script.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo ip link set can0 down 
-sudo ip link set can0 type can bitrate 1000000
+sudo ip link set can0 type can bitrate 250000
 sudo ip link set can0 up
 
 tmux new-session -d bash
